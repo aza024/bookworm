226658f5bdbaeb3ff163018a233b00b06f0b3922
Andrea  Piazza <apiazza023@gmail.com>
2019-06-01 12:48:54 -0700

    completed filestructure

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('testing')
+// server.js
+const express = require('express');
+const app = express(); 
+
+app.get('/',(req, res) => res.send('hello'))
+
+const server = app.listen(3000);
